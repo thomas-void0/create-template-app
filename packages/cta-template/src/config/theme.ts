c1db177ee0db59195af5f6d8d7d67b031f5b937d
newrankyangjingyu <yjy15680489038@163.com>
2023-01-12 18:01:01 +0800

    chore: 删除模板文件

--- packages/cta-template/src/config/theme.ts
+++
@@ -1,9 +0,0 @@
-// antd主题样式
-export default {
-	token: {
-		colorPrimary: 'rgb(114, 127, 255)',
-		colorSplit: 'rgb(225, 230, 230)',
-		colorLink: 'rgb(0, 136, 255)',
-		colorTextLabel: 'rgb(24, 26, 26)'
-	}
-}
--- packages/cta-template/src/core/index.ts
+++
@@ -1,8 +0,0 @@
-import Create from './Create'
-import Access, { AccessProps } from './Access'
-import { useGlobal } from './context/global'
-import useAccess from './Access/useAccess'
-
-export { Create, useGlobal, Access, useAccess }
-
-export type { AccessProps }
