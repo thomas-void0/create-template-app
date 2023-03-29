09ffc3affa4b1ee72b2c2e16a626c24f755d7ee4
newrankyangjingyu <yjy15680489038@163.com>
2023-03-29 15:12:12 +0800

    feat: add gitignore template

--- packages/create-template-app/src/lib/generateGit.ts
+++ packages/create-template-app/src/lib/generateGit.ts
@@ -1,6 +1,33 @@
 import { exec } from "child_process";
 import fs from "fs";
 
+const gitignoreTemplate = `
+# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+pnpm-debug.log*
+lerna-debug.log*
+
+node_modules
+dist
+dist-ssr
+*.local
+
+# Editor directories and files
+.vscode/*
+!.vscode/extensions.json
+.idea
+.DS_Store
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?
+`;
+
 function generateGit(): Promise<string> {
   return new Promise((resolve, reject) => {
     const existGit = fs.existsSync(".git");
@@ -11,6 +38,7 @@ function generateGit(): Promise<string> {
       if (err) {
         return reject("Error: git init failed.");
       }
+      fs.writeFileSync(".gitignore", gitignoreTemplate);
       resolve("completed.");
     });
   });
